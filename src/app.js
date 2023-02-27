fb2ce384bd405e9d2b84f7fcdcd8e86fe06f1364
francis <jerylfranz460@gmail.com>
2023-02-27 16:52:36 +0400

    FIX: ai responds properly if user clicks on a grid to trigger enemey attack

--- src/app.js
+++ src/app.js
@@ -1,98 +1,101 @@
-import './style.css'
-import { Display, Scenes } from './dom.js'
-import { Players, Ships, Ai } from './entities.js'
-import { GameBoard } from './game-board.js'
-import PubSub from 'pubsub-js'
+import "./style.css";
+import { Display, Scenes } from "./dom.js";
+import { Players, Ships, Ai } from "./entities.js";
+import { GameBoard } from "./game-board.js";
+import PubSub from "pubsub-js";
 
-const MAIN_CONT = document.getElementById('main-container');
+const MAIN_CONT = document.getElementById("main-container");
 const DP = new Display();
-const SC = new Scenes()
-const GB = new GameBoard()
-const PLAYER_ARR = GB.getPlayers(Players, Ai, Ships)
-PLAYER_ARR.forEach(player => {
-  player.createShipDock()
-})
+const SC = new Scenes();
+const GB = new GameBoard();
+const PLAYER_ARR = GB.getPlayers(Players, Ai, Ships);
+PLAYER_ARR.forEach((player) => {
+	player.createShipDock();
+});
 let isTurn = true;
 let isGame = false;
 let isSelection = false;
-MAIN_CONT.addEventListener('click', e => {
-  const target = e.target;
-  console.log(target)
-  change(e)
-  if (isTurn && target.classList.contains('AI-ship-cell')) {
-    console.log(target)
-    GB.receiveAttack(target)
-    isTurn = false
-  }
-  if (!isTurn) {
-    setTimeout(() => {
-      const AiTarget = PLAYER_ARR[1].attack()
-      GB.receiveAttack(AiTarget)
-      isTurn = true;
-    }, 300)
-  }
-})
-
-PLAYER_ARR[0].shipDock.forEach(ships => {
-  PubSub.subscribe("TargetCell", (msg, data) => {
-    ships.isHit(data)
-  })
-});
-PLAYER_ARR[1].shipDock.forEach(ships => {
-  PubSub.subscribe("TargetCell", (msg, data) => {
-    ships.isHit(data)
-  })
+MAIN_CONT.addEventListener("click", (e) => {
+	const target = e.target;
+	change(e);
+	if (
+		target.classList.contains("AI-ship-cell") ||
+		target.classList.contains("grids")
+	) {
+		console.log(target);
+		GB.receiveAttack(target);
+		isTurn = false;
+	}
+	if (!isTurn) {
+		console.log("enemy turn");
+		setTimeout(() => {
+			const AiTarget = PLAYER_ARR[1].attack();
+			GB.receiveAttack(AiTarget);
+			isTurn = true;
+		}, 300);
+	}
 });
 
-PubSub.subscribe('getWinner', (msg, data) => {
-  isGame = false;
-  SC.gameOver()
-  DP.displayWinner(msg, data)
-})
-PubSub.subscribe('shipSunkAI', (msg, data) => {
-  PLAYER_ARR[1].removeShip(data)
-  console.log(GB.evalPlayers())
-  console.log({ this: PLAYER_ARR[0], dock: PLAYER_ARR[0].shipDock })
-  console.log({ this: PLAYER_ARR[1], dock: PLAYER_ARR[1].shipDock })
-})
+PLAYER_ARR[0].shipDock.forEach((ships) => {
+	PubSub.subscribe("TargetCell", (msg, data) => {
+		ships.isHit(data);
+	});
+});
+PLAYER_ARR[1].shipDock.forEach((ships) => {
+	PubSub.subscribe("TargetCell", (msg, data) => {
+		ships.isHit(data);
+	});
+});
 
-PubSub.subscribe('shipSunkPlayer', (msg, data) => {
-  PLAYER_ARR[0].removeShip(data)
-  console.log(GB.evalPlayers())
-  console.log({ this: PLAYER_ARR[0], dock: PLAYER_ARR[0].shipDock })
-  console.log({ this: PLAYER_ARR[1], dock: PLAYER_ARR[1].shipDock })
-})
+PubSub.subscribe("getWinner", (msg, data) => {
+	isGame = false;
+	SC.gameOver();
+	DP.displayWinner(msg, data);
+});
+PubSub.subscribe("shipSunkAI", (msg, data) => {
+	PLAYER_ARR[1].removeShip(data);
+	console.log(GB.evalPlayers());
+	console.log({ this: PLAYER_ARR[0], dock: PLAYER_ARR[0].shipDock });
+	console.log({ this: PLAYER_ARR[1], dock: PLAYER_ARR[1].shipDock });
+});
 
+PubSub.subscribe("shipSunkPlayer", (msg, data) => {
+	PLAYER_ARR[0].removeShip(data);
+	console.log(GB.evalPlayers());
+	console.log({ this: PLAYER_ARR[0], dock: PLAYER_ARR[0].shipDock });
+	console.log({ this: PLAYER_ARR[1], dock: PLAYER_ARR[1].shipDock });
+});
 
 async function change(e) {
-  const target = e.target
-  if (target.matches('#start-btn')) {
-    isSelection = true
-    PubSub.publish('dropShips', isSelection)
-    DP.changeScene(SC.selectionScene(GB.createBoard(10, 10)), true).then(data => {
-      DP.shipToDock(PLAYER_ARR[0].shipDock, data.docks)
-    })
-  }
-  if (target.matches('#play-btn')) {
-    window.location.reload()
-  }
-  if (target.matches('#ready-btn')) {
-    let AIGrid = DP.appendGrid(GB.createBoard(10, 10), 'ai-grid-cont')
-    DP.changeScene(SC.gameScene(AIGrid), true).then(data => {
-      data.gridsCont.appendChild(AIGrid)
-      PLAYER_ARR[1].placeShip(AIGrid)
-    })
-  }
+	const target = e.target;
+	if (target.matches("#start-btn")) {
+		isSelection = true;
+		PubSub.publish("dropShips", isSelection);
+		DP.changeScene(SC.selectionScene(GB.createBoard(10, 10)), true).then(
+			(data) => {
+				DP.shipToDock(PLAYER_ARR[0].shipDock, data.docks);
+			}
+		);
+	}
+	if (target.matches("#play-btn")) {
+		window.location.reload();
+	}
+	if (target.matches("#ready-btn")) {
+		let AIGrid = DP.appendGrid(GB.createBoard(10, 10), "ai-grid-cont");
+		DP.changeScene(SC.gameScene(AIGrid), true).then((data) => {
+			data.gridsCont.appendChild(AIGrid);
+			PLAYER_ARR[1].placeShip(AIGrid);
+		});
+	}
 }
-
 
 // draggTarget
 
-MAIN_CONT.addEventListener('dragstart', DP.dragShip)
-MAIN_CONT.addEventListener('dragover', DP.onDragShip)
-MAIN_CONT.addEventListener('drop', (e) => {
-  let target = e
-  if (e.target.matches('.grids')) {
-    DP.dropShip(target)
-  }
-})
+MAIN_CONT.addEventListener("dragstart", DP.dragShip);
+MAIN_CONT.addEventListener("dragover", DP.onDragShip);
+MAIN_CONT.addEventListener("drop", (e) => {
+	let target = e;
+	if (e.target.matches(".grids")) {
+		DP.dropShip(target);
+	}
+});
